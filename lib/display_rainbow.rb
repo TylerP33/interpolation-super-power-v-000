a25ab4a2660aa239014dc286320585ab73d25ca0
Tyler Pulse <tyler3321@gmail.com>
2017-10-05 21:54:17 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,4 @@
 color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 letters = ["R", "O", "Y", "G", "B", "I", "V"]
 
-def display_rainbow('red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet')
-  puts "#{letter}: #{color}"
-end
+
